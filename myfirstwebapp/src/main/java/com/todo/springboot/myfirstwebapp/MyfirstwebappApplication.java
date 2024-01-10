daafd11cc43474e8bee589881ecf09caacad0e02
Oybek <oybek@inbl.io>
2024-01-10 09:30:16 +0900

    login page started code boilerplate commit

---
+++ myfirstwebapp/src/main/java/com/todo/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,14 @@
+package com.todo.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappApplication.class, args);
+		System.out.println("App started");
+	}
+
+}
---
+++ myfirstwebapp/src/main/webapp/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>This is my first title</title>
+</head>
+<body>
+This is my first HTML body
+</body>
+</html>
